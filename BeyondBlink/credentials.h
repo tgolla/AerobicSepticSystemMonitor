8d54e88555fecabff56f78b79677b21d8d53ff7e
Terence Golla <{ID}+{username}@users.noreply.github.com>
2024-02-09 18:20:41 -0600

    Adding base beyond blink code.

---
+++ BeyondBlink/credentials.h
@@ -0,0 +1,15 @@
+// Note that this file was created without actual passwords, commited to Git and
+// then set to be excluded from being checked from future changes with the following command.
+//
+//   git update-index --assume-unchanged credentials.h           
+//
+// https://arduino.stackexchange.com/questions/40411/hiding-wlan-password-when-pushing-to-github/55583#55583
+
+#ifndef CREDENTIALS_H
+#define CREDENTIALS_H
+
+// Replace with your actual SSID and password:
+#define WIFI_SSID "Your WiFI SSID here"
+#define WIFI_PASSWORD "Your WiFi password here"
+
+#endif
